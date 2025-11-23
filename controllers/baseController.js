afbc618d9ba1486b755a6847e136601082bfa155
Junnot Daniel <jerenasonjunnot@gmail.com>
2025-11-23 15:15:06 +0300

    Remove log

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -4,8 +4,6 @@ const baseController = {};
 baseController.buildHome = async function (req, res) {
   const nav = await utilities.getNav();
 
-  console.log(nav);
-
   res.render("index", { title: "Home", nav });
 };
 
